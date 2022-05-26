8680147bc872870effb6418a4b39da123c89af36
Jordan Bank <jordanbank@Jordans-MacBook-Air.local>
2022-05-26 11:31:55 -0400

    Added react

---
+++ frontend/breakfast/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg'
+import './App.css'
+import {useState, useEffect} from 'react'
+import axios from 'axios'
+
+const App = () => {
+  return (
+    <>
+    <p>Hello!</p>
+    </>
+  )
+}
+
+
+export default App
