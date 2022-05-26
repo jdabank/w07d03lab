3619c6c4225fea0c3561b771105ac343e9698087
CANDACE HARRIS <candaceharris@CANDACEs-MBP.lan>
2022-05-26 14:34:40 -0400

    delete button

--- frontend/breakfast/src/App.js
+++ frontend/breakfast/src/App.js
@@ -1,37 +1,53 @@
-import logo from './logo.svg'
-import './App.css'
-import {useState, useEffect} from 'react'
-import axios from 'axios'
+import logo from "./logo.svg";
+import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const App = () => {
-  const [newName, setNewName] = useState('')
-  const [newImage, setNewImage] = useState('')
-  const [newCalories, setNewCalories] = useState('')
-  const [newHad, setNewHad] = useState('false')
-  const [newVeg, setNewVeg] = useState('false')
-  const [newGood, setNewGood] = useState('false')
+  const [newName, setNewName] = useState("");
+  const [newImage, setNewImage] = useState("");
+  const [newCalories, setNewCalories] = useState("");
+  const [newHad, setNewHad] = useState("false");
+  const [newVeg, setNewVeg] = useState("false");
+  const [newGood, setNewGood] = useState("false");
 
-  const [breakfast, setBreakfast] = useState([])
+  const [breakfast, setBreakfast] = useState([]);
+
+  const handleDelete = (breakfastDelete) => {
+    axios
+      .delete("http://localhost:3000/breakfast/${breakfastData._id}")
+      .then(() => {
+        axios.get("http://localhost:3000/breakfast").then((response) => {
+          setBreakfast(response.data);
+        });
+      });
+  };
 
   useEffect(() => {
-    axios.get('http://localhost:3000/breakfast').then((response) => {
-      setBreakfast(response.data)
-      console.log(response.data[0].name)
-    })
-  }, [])
+    axios.get("http://localhost:3000/breakfast").then((response) => {
+      setBreakfast(response.data);
+      console.log(response.data[0].name);
+    });
+  }, []);
 
   return (
     <>
-    {breakfast.map((breakfast) => {
-      return (
-      <>
-      <p>{breakfast.name}</p>
-      </>
-      )
-    })}
+      {breakfast.map((breakfast) => {
+        return (
+          <>
+            <p>{breakfast.name}</p>
+            <button
+              onClick={(event) => {
+                handleDelete(breakfast.name);
+              }}
+            >
+              Delete
+            </button>
+          </>
+        );
+      })}
     </>
-  )
-}
+  );
+};
 
-
-export default App
+export default App;
