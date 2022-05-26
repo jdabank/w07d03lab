997a8cfb0b50b2c7b21d2e79ce655d295d7e74fb
Jordan Bank <jordanbank@Jordans-MacBook-Air.local>
2022-05-26 13:52:19 -0400

    got add working

--- frontend/breakfast/src/App.js
+++ frontend/breakfast/src/App.js
@@ -13,22 +13,81 @@ const App = () => {
 
   const [breakfast, setBreakfast] = useState([])
 
+  const addName = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const addImage = (event) => {
+    setNewImage(event.target.value)
+  }
+
+  const addCalories = (event) => {
+    setNewCalories(event.target.value)
+  }
+
+  const addHad = (event) => {
+    setNewHad(event.target.checked)
+  }
+
+  const addVeg = (event) => {
+    setNewVeg(event.target.checked)
+  }
+
+  const addGood = (event) => {
+    setNewGood(event.target.checked)
+  }
+
+  const addBreakfast = (event) => {
+    event.preventDefault()
+    axios.post('http://localhost:3000/breakfast', {
+      name: newName,
+      image: newImage,
+      calories: newCalories,
+      hadThisWeek: newHad,
+      vegetarian: newVeg,
+      good: newGood,
+    }).then(() => {
+      axios.get('http://localhost:3000/breakfast').then((response) => {
+        setBreakfast(response.data)
+      })
+    })
+  }
+
+
+
   useEffect(() => {
     axios.get('http://localhost:3000/breakfast').then((response) => {
       setBreakfast(response.data)
-      console.log(response.data[0].name)
     })
   }, [])
 
   return (
     <>
+    <h1>Breakfast Bar</h1>
+    <section>
+      <h3>Add a New Meal</h3>
+      <form onSubmit = {addBreakfast}>
+      <input type = 'text' placeholder = 'Name' onChange = {addName}/><br/>
+      <input type = 'text' placeholder = 'Image' onChange = {addImage}/><br/>
+      <input type = 'text' placeholder = 'Calories' onChange = {addCalories}/><br/>
+      Did you already have it this week? <input type = 'checkbox' onChange = {addHad}/><br/>
+      Is it Vegetarian? <input type = 'checkbox' onChange = {addVeg}/><br/>
+      Was it good? <input type = 'checkbox' onChange = {addGood}/><br/>
+      <input type = 'submit' value = 'Submit'/>
+      </form>
+    </section>
+    <section>
+    <h3>All Breakfasts</h3>
     {breakfast.map((breakfast) => {
       return (
       <>
       <p>{breakfast.name}</p>
+      <img src = {breakfast.image}/>
+      <p>{breakfast.calories} Calories</p>
       </>
       )
     })}
+    </section>
     </>
   )
 }
