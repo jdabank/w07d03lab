c605ccebbe112a1bae3c8472613511ab0aea8465
Jordan Bank <jordanbank@Jordans-MacBook-Air.local>
2022-05-26 12:08:19 -0400

    added return, connected to backend

--- frontend/breakfast/src/App.js
+++ frontend/breakfast/src/App.js
@@ -4,9 +4,31 @@ import {useState, useEffect} from 'react'
 import axios from 'axios'
 
 const App = () => {
+  const [newName, setNewName] = useState('')
+  const [newImage, setNewImage] = useState('')
+  const [newCalories, setNewCalories] = useState('')
+  const [newHad, setNewHad] = useState('false')
+  const [newVeg, setNewVeg] = useState('false')
+  const [newGood, setNewGood] = useState('false')
+
+  const [breakfast, setBreakfast] = useState([])
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/breakfast').then((response) => {
+      setBreakfast(response.data)
+      console.log(response.data[0].name)
+    })
+  }, [])
+
   return (
     <>
-    <p>Hello!</p>
+    {breakfast.map((breakfast) => {
+      return (
+      <>
+      <p>{breakfast.name}</p>
+      </>
+      )
+    })}
     </>
   )
 }
